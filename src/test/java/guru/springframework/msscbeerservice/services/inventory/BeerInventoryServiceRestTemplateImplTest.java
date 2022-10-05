a5d081ab5b7601cd2db667eaf7d20b9d64460a6d
Graham Bygrave <graham@bygrave.org>
2022-10-05 11:00:33 +0100

    Refactored model (JSONable) objects to common package. Closes #6.

--- src/test/java/guru/springframework/msscbeerservice/services/inventory/BeerInventoryServiceRestTemplateImplTest.java
+++ src/test/java/guru/springframework/msscbeerservice/services/inventory/BeerInventoryServiceRestTemplateImplTest.java
@@ -1,18 +1,12 @@
 package guru.springframework.msscbeerservice.services.inventory;
 
-import guru.springframework.msscbeerservice.bootstrap.BeerLoader;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 // @Disabled
-@SpringBootTest
+// @SpringBootTest
 class BeerInventoryServiceRestTemplateImplTest {
 
     @Autowired
