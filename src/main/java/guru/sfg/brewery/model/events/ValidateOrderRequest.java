b68f11abbc182156b46f8e1f50e33330a34d83fe
Graham Bygrave <graham@bygrave.org>
2022-10-12 11:33:06 +0100

    Needs noargs ctor to deserialise from JMS queue.

--- src/main/java/guru/sfg/brewery/model/events/ValidateOrderRequest.java
+++ src/main/java/guru/sfg/brewery/model/events/ValidateOrderRequest.java
@@ -4,11 +4,13 @@ import guru.sfg.brewery.model.BeerOrderDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class ValidateOrderRequest implements Serializable {
     private static final long serialVersionUID = 8695309424794727221L;
